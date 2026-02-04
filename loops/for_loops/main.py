4669408ef8c8abaf9e6ca6014cf0f26a0a29c6aa
dhifanhisyam-lang <>
2026-02-04 07:16:50 +0000

    For Loops

---
+++ loops/for_loops/main.py
@@ -0,0 +1,10 @@
+grocery = {
+    "Milk": 2.99,
+    "Eggs": 3.49,
+    "Bread": 1.99,
+    "Apples": 0.89
+}
+
+print("List of groceries:")
+for item, price in grocery.items():
+    print(f"{item} {price}")
