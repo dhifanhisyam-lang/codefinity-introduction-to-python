63312b95119fc304a141bfa0702a99aaf229b847
dhifanhisyam-lang <>
2026-02-04 07:52:27 +0000

    While loops

---
+++ loops/while_loops/main.py
@@ -0,0 +1,12 @@
+# Simulate a grocery store restocking process
+stock = 12  # Current stock level
+restock_goal = 20 # Desired stock level
+restock_amount = 2
+# Your code here
+while stock < restock_goal:
+   restock_amount = 2
+   stock += restock_amount
+   print(f"Current stock amount: {stock}")
+print("Final stock value 20")   
+print("Restocking complete!")
+
